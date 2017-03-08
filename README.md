2d70a1dd97fc199742b6b60241bd9d189c19cdae
Stephan Sum <sum@live.de>
2017-03-08 21:43:42 +0100

    init

--- README.md
+++ README.md
@@ -1,30 +1,5 @@
-# boilerplate-frontend
+# local weather app
 
-a lightweight template for simple frontend sites
+this local weather app was an assignment for freecodecamp's frontend certification.
 
-
-### features
-
-- JS bundling provided by the webpack core
-- SASS transpilation: sass-loader transforms SASS into CSS. css-loader parses the CSS into JS and resolves any dependencies. style-loader outputs our CSS into a style tag in the document.
-- JS + CSS injection (html-webpack-plugin)
-- JS sourcemaps (provided by the "devtool" property)
-- "webpack-dev-server" provides live-reloading
-- "copy-webpack-plugin" selects files that need to be copied from src to dist (https://github.com/kevlened/copy-webpack-plugin)
-- "write-file-webpack-plugin" is needed to force webpack-dev-server to copy watched files to dist folder instead from only serving them up from memory. files will still get served from memory but also copied. (https://github.com/gajus/write-file-webpack-plugin)
-- "optimize-css-assets-webpack-plugin" minifies css. this module is necessary as webpack isnt able to minify css anymore, when we dont inject css via js but instead use the "extract-text-webpack-plugin"
-- "file-loader" and "url-loader" are resolving image references in css files
-
-### todo
-
-- linting
-- bugfix: flash of unstyled content due to css injection; maybe use ExtractTextWebpackPlugin  in production
-
-### other ressources
-
-https://webpack.js.org/guides/  
-https://webpack.js.org/configuration/  
-http://javascriptplayground.com/blog/2016/10/moving-to-webpack-2/  
-https://www.sitepoint.com/beginners-guide-to-webpack-2-and-module-bundling/  
-https://github.com/NMFR/optimize-css-assets-webpack-plugin  
-https://survivejs.com/webpack/loading/images/
+[Demo](https://blackmesacode.github.io/freecodecamp-localweather/)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-import './custom.scss'
+import "../node_modules/weather-icons/css/weather-icons.min.css";
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import './custom.scss';
+
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;  // make jQuery globally available
+
+
+// main entry point
+
+$(() => {
+    getUserLocationByIP().then(function(data) {
+        getWeather(new OpenWeatherMapUrl(data.city).combined).done(function(weather) {
+            loadIconMappings().done(function(mapping) {
+                var iconName = getIconName(weather, mapping);
+                updateWeatherIcon(iconName);
+            })
+        });
+    });
+});
+
+
+
+
+function loadIconMappings() {
+    return $.getJSON("icons.json", function(json) {} );
+}
+
+
+function getUserLocationByIP() {
+    return $.get("http://ipinfo.io", function() {}, "jsonp");
+}
+
+function OpenWeatherMapUrl(city) {
+    this.base = "http://api.openweathermap.org/data/2.5/weather?q=";
+    this.units = "&units=metric";
+    this.appid = "&APPID=061f24cf3cde2f60644a8240302983f2";
+    this.combined = this.base + city + this.units + this.appid;
+}
+
+function getWeather(url) {
+    return $.get(url, function(response) { }, "jsonp");
+}
+
+function updateWeatherIcon(icon) {
+    $("#weather-icon").removeClass().addClass(icon);
+}
+
+function getIconName(weather, mapping) {  // credits to https://gist.github.com/tbranyen/62d974681dea8ee0caa1#file-icons-json for the provided mapping
+    var prefix = 'wi wi-';
+    var code = weather.weather[0].id;
+    var icon = mapping[code].icon;
+
+    // If we are not in the ranges mentioned above, add a day/night prefix.
+    if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+        icon = 'day-' + icon;
+    }
+
+    // Finally tack on the prefix.
+    return prefix + icon;
+}
